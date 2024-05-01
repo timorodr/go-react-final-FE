c4886452d0c9adfa6941d3981f31601e6d81ef93
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-05-01 18:57:37 -0500

    deployment update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,14 +5,14 @@ const Nav = () => {
         <div>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="http://localhost:3000/">Home</a>
+                <a className="navbar-brand" href="https://hilarious-biscotti-0d1872.netlify.app/">Home</a>
 
                 <ul className="navbar-nav me-auto mb-2 mb-md-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="http://localhost:3000/login">Login</a>
+                    <a className="nav-link active" aria-current="page" href="https://hilarious-biscotti-0d1872.netlify.app/login">Login</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="http://localhost:3000/signup">Register</a>
+                    <a className="nav-link" href="https://hilarious-biscotti-0d1872.netlify.app/signup">Register</a>
                     </li>
                 </ul>
 
