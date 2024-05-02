495b47b08dd19851135c8ca6c6f429f6e93e84ff
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-05-01 21:41:26 -0500

    api key

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,35 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
 
 
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
 
-    const home = () => {
-        navigate(`/`); // Navigate away when done
-    };
+    // const home = () => {
+    //     navigate(`/`); // Navigate away when done
+    // };
 
-    const login = () => {
-        navigate(`/login`); // Navigate away when done
-    };
-    const signup = () => {
-        navigate(`/signup`); // Navigate away when done
-    };
+    // const login = () => {
+    //     navigate(`/login`); // Navigate away when done
+    // };
+    // const signup = () => {
+    //     navigate(`/signup`); // Navigate away when done
+    // };
 
     return (
         <div>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="container-fluid">
-                <button className="navbar-brand" onClick={home}>Home</button>
+                <a className="navbar-brand" href="/landing">Home</a>
 
                 <ul className="navbar-nav me-auto mb-2 mb-md-0">
                     <li className="nav-item">
-                    <button className="nav-link active" aria-current="page" onClick={login}>Login</button>
+                    <a className="nav-link active" aria-current="page" href="/login">Login</a>
                     </li>
                     <li className="nav-item">
-                    <button className="nav-link" onClick={signup}>Register</button>
+                    <a className="nav-link" href="/signup">Register</a>
                     </li>
                 </ul>
 
